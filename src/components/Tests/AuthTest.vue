1db84469061751dc01827070da4848dd81f9211b
Jhonatan Froeder <froeder3@gmail.com>
2018-06-27 08:30:06 -0400

    included auth test

---
+++ src/components/Tests/AuthTest.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-conainer>
+        <v-layout>
+            <v-card>
+                <v-card-title>Auth Test</v-card-title>
+            </v-card>
+        </v-layout>
+    </v-conainer>
+</template>
